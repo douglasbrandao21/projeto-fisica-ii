71c4e578051f6b927cdccf0b959081eee31626c5
Gabriel Gava <nero.gava@gmail.com>
2021-02-19 14:10:59 -0300

    Starting Project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Projeto de Física II: <code>Exercicio 4</code>.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
